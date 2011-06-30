990f888ba4b9bf4b1658d7a8fb580db3e18d0892
Ben Foxall <benfoxall@gmail.com>
2011-06-30 22:54:34 +0100

    working more like I want it to. Removed some unused cruft

--- code.js
+++ code.js
@@ -77,13 +77,18 @@ var timestamps = list({
 			this.redraw();
 			
 			this.next() && this.next().redraw();
-			this.prev() && this.prev().redraw();
+			this.prev() && this.prev().redraw && this.prev().redraw();
 		}
-		return this.$artists;
+		return (this.$artists || {});
 	},
 	deltas: function(ts){
-		var from = ts && ts.artists ? ts.artists() || {} : {};
-		var to = this.artists() || {};
+		var from = this.artists();
+		var to = {};
+		if(ts && ts.artists){
+			to = ts.artists();
+		}
+		
+		// var to = ts && ts.artists ? ts.artists() || {} : {};
 		
 		return _({}).chain()
 			.extend(from,to)
@@ -105,6 +110,22 @@ var timestamps = list({
 });
 
 
+// gives a colour that is the result of hashing the key
+// very horrible at the moment, looks and implemenation
+var color = function(key){
+	var h = 0;
+	for (var i=0; i < key.length; i++) {
+		h = (h + key.charCodeAt(i)) % 768;
+	};
+	
+	var r = parseInt(Math.abs(h < 512 ? h : (h - 768)));
+	var g = parseInt(Math.abs(h - 256));
+	var b = parseInt(Math.abs(h - 512));
+	
+	return "rgba("+Math.min(r,256)+","+Math.min(g,256)+","+Math.min(b,256)+",0.9)"
+	
+}
+
 
 var view = function(){
 	var width = 180;
@@ -115,117 +136,47 @@ var view = function(){
 	
 	var scale = 1;
 	
-	return function(t){
+	return function(timestamp){
+		
 		//TODO check for support
-		var timestamp = t;
-		//console.log("VIEW", timestamp);
 		var ctx = canvas.get(0).getContext('2d');
 		
+		//clear the canvas
 		ctx.fillStyle = 'white';
 		ctx.fillRect(0,0,width,height);
 		
+		
+		var draw = function(data, x1, x2){
+			var y1 = 0; var y2 = 0;
+			
+			_(data).each(function(d){
+				var artist = d[0], from = d[1], to = d[2];
+				
+				var exit_y = (from + to)/2;
+				
+				ctx.fillStyle = color(artist);
+				
+				ctx.beginPath();
+				ctx.moveTo(x1,y1);
+				ctx.lineTo(x1,y1 + from);
+
+				ctx.lineTo(x2,y2 + exit_y);
+				ctx.lineTo(x2,y2);
+				ctx.fill();
+
+				y1 += from;
+				y2 += exit_y;
+				
+			});
+			
+		};
+		
 		var left = timestamp.deltas(timestamp.prev());
-		//console.log(left);
-		
-		var x = 0;
-		var x2 = width / 2;
-		
-		
-		var from_total = 0, to_total = 0;
-		
-		_(left).each(function(d){
-			var artist = d[0];
-			var from = d[1];
-			var to = d[2];
-			
-			var from2 = (from + to)/2;
-			
-			
-			ctx.fillStyle = color(artist);
-			
-			ctx.beginPath();
-			ctx.moveTo(x,from_total);
-			ctx.lineTo(x,from_total + from2);
-			
-			ctx.lineTo(x2,to_total + to);
-			ctx.lineTo(x2,to_total);
-			ctx.fill();
-			
-			from_total += from2;
-			to_total += to;
-			
-			
-		});
-		
-		
+		draw(left, width/2, 0);
 		
 		var right = timestamp.deltas(timestamp.next());
-		//console.log(left);
+		draw(right, width/2, width);
 		
-		var x = width/2;
-		var x2 = width;
-		
-		var from_total = 0, to_total = 0;
-		
-		_(right).each(function(d){
-			var artist = d[0];
-			var from = d[2];
-			var to = d[1];
-			
-			var to2 = (from + to)/2;
-			
-			
-			ctx.fillStyle = color(artist);
-			
-			ctx.beginPath();
-			ctx.moveTo(x,from_total);
-			ctx.lineTo(x,from_total + from);
-			
-			ctx.lineTo(x2,to_total + to2);
-			ctx.lineTo(x2,to_total);
-			ctx.fill();
-			
-			from_total += from;
-			to_total += to2;
-			
-			
-		});
-		
-		
-        // var right = timestamp.deltas(timestamp.next());
-        // console.log(right);
-        // 
-        // var x = width;
-        // var x2 = width / 2;
-        // 
-        // var y1 = 0, y2 = 0;
-        // _(right).each(function(d){
-        //  var artist = d[0];
-        //  var from = d[1];
-        //  var to = d[2];
-        //  
-        //  var m = (from + to) / 2;
-        //  
-        //  
-        //  ctx.fillStyle = color(artist);
-        //  
-        //  var y1diff = scale * from;
-        //  var y2diff = scale * to;
-        //  
-        //  ctx.beginPath();
-        //  ctx.moveTo(x,y1);
-        //  ctx.lineTo(x,y1 + y1diff);
-        //  ctx.lineTo(x2,y2 + y2diff);
-        //  ctx.lineTo(x2,y2);
-        //  ctx.lineTo(x,y1);
-        //  ctx.fill();
-        //  
-        //  y1 += y1diff;
-        //  y2 += y2diff;
-        //  
-        //  
-        // })
-	
 	};
 }
 
@@ -320,189 +271,8 @@ var fetch = function(username,count){
 			
 		});
 		
-		/*var sem = count;
-		
-		for (var i=0; i < count; i++) {
-			var timespan = chart.pop();
-			
-			if(!timespan){
-			  break;
-			};
-			
-			var handle = function(){
-				var num = i;
-				return function(data){
-					try{
-					//Run some code here
-						var artists = data.weeklyartistchart.artist;
-						
-						artists.sort(function(a,b){
-							return a.name < b.name ? -1 : 1;
-						})
-						
-						chart_data[num] = $.map(artists, function(a){
-							return {name:a.name, plays:a.playcount};
-						});
-					
-					} catch(e) {	
-						chart_data[num] = [];
-						err('Unable to process');
-					}
-					
-					sem--;
-					$('#times').text(sem + " left");
-					
-					if(!sem){
-						render();
-						$('#times').text("");
-					}
-				}
-			}();
-			
-			
-			last_fm('user.getweeklyartistchart', {user:username, from:timespan.from, to:timespan.to}).done(handle)
-			
-		};*/
-		
 	})
 	
-};
-
-
-// the logic here is really hard to explain without a pen and paper/wall.
-// though essentially it gives the difference between play counts at a 
-// given interval.
-//
-// I'm sure that there is a lovely way to do this, and it may involve a
-// more appropriate data structure.  The main advantage here is that it
-// won't be 'too' affected by the number of unique artists over time.
-var deltas = function(){
-	var prev = [];
-	return _(chart_data).map(function(curr){
-		var output = [];
-		
-		var i = 0, j = 0;
-		while(i < prev.length || j < curr.length){
-			var A = prev[i];
-			var B = curr[j];
-			
-			if(!A && !B){
-				break;
-				
-			} else if(!A || (B && A.name > B.name)){
-				//use B
-				output.push({
-					name:B.name,
-					from:0,
-					to:parseInt(B.plays)
-				});
-				j++;
-			} else if (!B || (A && B.name > A.name)){
-				//use A
-				output.push({
-					name:A.name,
-					from:parseInt(A.plays),
-					to:0
-				});
-				i++;
-			} else {
-				
-				output.push({
-					name:A.name,
-					from:parseInt(A.plays),
-					to:parseInt(B.plays)
-				});
-				i++;
-				j++;
-			}
-		}
-		
-		prev = curr;
-		
-		return output;
-		
-	});
-}
-
-// gives a colour that is the result of hashing the key
-// very horrible at the moment, looks and implemenation
-var color = function(key){
-	var h = 0;
-	for (var i=0; i < key.length; i++) {
-		h = (h + key.charCodeAt(i)) % 768;
-	};
-	
-	var r = parseInt(Math.abs(h < 512 ? h : (h - 768)));
-	var g = parseInt(Math.abs(h - 256));
-	var b = parseInt(Math.abs(h - 512));
-	
-	return "rgba("+Math.min(r,256)+","+Math.min(g,256)+","+Math.min(b,256)+",0.6)"
-	
-}
-
-// The actual drawing stuff - took a few different approaches here (it shows).
-var render = function(){
-/*	
-	var interval_width = 180;
-	
-	var width = chart_data.length * interval_width;
-	var height = 600;
-	
-	//possibly quite expensive
-	var chart_deltas = deltas();
-	
-	var totals = _(chart_deltas).map(function(data){
-		var values = _(data).map(function(d){
-			// not quite right, though pessimistic, so will do for now
-			return Math.max(d.from,d.to); 
-		});
-		return _(values).sum();
-	})	
-	var max = _(totals).max();
-	
-	var scale = height / max;
-	
-	$("#display").html('<canvas height="'+height+'" width="'+width+'" id="canvas"></canvas>');
-	var canvas = $('canvas').get(0);
-	
-	
-	if (canvas.getContext){
-		
-		var ctx = canvas.getContext('2d');
-		var x = 0;
-		
-		
-		_(chart_deltas).each(function(chart_delta){
-			
-			var y1 = 0, y2 = 0;
-			_(chart_delta).each(function(delta){
-				
-				ctx.fillStyle = color(delta.name);
-				
-				var y1diff = scale * delta.from;
-				var y2diff = scale * delta.to;
-				
-				ctx.beginPath();
-				ctx.moveTo(x,y1);
-				ctx.lineTo(x,y1 + y1diff);
-				ctx.lineTo(x + interval_width,y2 + y2diff);
-				ctx.lineTo(x + interval_width,y2);
-				ctx.lineTo(x,y1);
-				ctx.fill();
-				
-				
-				y1 += y1diff;
-				y2 += y2diff;
-			})
-			
-			x += interval_width;
-			
-		});
-		
-	} else {
-		err('No Canvas Support');
-	};
-	*/
 };
 
 
